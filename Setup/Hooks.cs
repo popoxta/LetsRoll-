30b9fc4df9cc4f8a3447ceddfb32d524b0f612ce
lari <popoxta@gmail.com>
2025-04-15 19:12:47 +1200

    Set up hooks for initializing browser for DI and destroying browser

---
+++ Setup/Hooks.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using OpenQA.Selenium;
+using Reqnroll;
+using Reqnroll.BoDi;
+
+namespace LetsRoll.Setup;
+
+[Binding]
+public sealed class Hooks(IObjectContainer container)
+{
+    [BeforeScenario]
+    public void SetupWebDriver()
+    {
+        var factory = new BrowserFactory(ConfigurationManager.AppSettings["browser"]);
+        container.RegisterInstanceAs(factory.Browser);
+    }
+
+    [AfterScenario]
+    public void DestroyWebDriver()
+    {
+        var driver = container.Resolve<IWebDriver>();
+        if (driver is null) return;
+        driver.Quit();
+        driver.Dispose();
+    }
+}
